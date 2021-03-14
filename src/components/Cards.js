2384d07a18024e98d1ba34c8f5ad4d7c9f7cd9f8
Andrijaj01 <andrijajovanovic001@gmail.com>
2021-03-14 16:04:53 +0100

    optimized images

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import './Cards.css';
 import CardItem from './CardItem';
@@ -10,13 +11,13 @@ function Cards() {
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
-              src='images/img-9.jpg'
+              src='images/img-9.webp'
               text='Search engine results scrapper'
               label='Python'
               path='/services'
             />
             <CardItem
-              src='images/img-2.jpg'
+              src='images/img-2.webp'
               text='reddit and discord bots'
               label='Automation'
               path='/services'
@@ -24,19 +25,19 @@ function Cards() {
           </ul>
           <ul className='cards__items'>
             <CardItem
-              src='images/img-3.jpg'
+              src='images/img-3.webp'
               text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
               label='Mystery'
               path='/services'
             />
             <CardItem
-              src='images/img-4.jpg'
+              src='images/img-4.webp'
               text='Experience Football on Top of the Himilayan Mountains'
               label='Adventure'
               path='/products'
             />
             <CardItem
-              src='images/img-8.jpg'
+              src='images/img-8.webp'
               text='Ride through the Sahara Desert on a guided camel tour'
               label='Adrenaline'
               path='/sign-up'
--- src/components/pages/Products.js
+++ src/components/pages/Products.js
@@ -12,13 +12,13 @@ export default function Products() {
       <div className='cards__wrapper'>
         <ul className='cards__items'>
           <CardItem
-            src='images/img-9.jpg'
+            src='images/img-9.webp'
             text='Explore the hidden waterfall deep inside the Amazon Jungle'
             label='Adventure'
             path='/services'
           />
           <CardItem
-            src='images/img-2.jpg'
+            src='images/img-2.webp'
             text='Travel through the Islands of Bali in a Private Cruise'
             label='Luxury'
             path='/services'
@@ -26,19 +26,19 @@ export default function Products() {
         </ul>
         <ul className='cards__items'>
           <CardItem
-            src='images/img-3.jpg'
+            src='images/img-3.webp'
             text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
             label='Mystery'
             path='/services'
           />
           <CardItem
-            src='images/img-4.jpg'
+            src='images/img-4.webp'
             text='Experience Football on Top of the Himilayan Mountains'
             label='Adventure'
             path='/products'
           />
           <CardItem
-            src='images/img-8.jpg'
+            src='images/img-8.webp'
             text='Ride through the Sahara Desert on a guided camel tour'
             label='Adrenaline'
             path='/sign-up'
