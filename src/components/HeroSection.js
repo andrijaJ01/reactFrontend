630aa2934307435bc5e3de21b3e0b141e418601c
Andrijaj01 <andrijajovanovic001@gmail.com>
2021-03-14 15:21:41 +0100

    fetch header title from m backedn

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -4,13 +4,41 @@ import { Button } from './Button';
 import './HeroSection.css';
 
 function HeroSection() {
+  const [data,setData]= useState({});
+  const [text,setText]=useState(localStorage.getItem('text') || '');
+  const [name,setName]=useState(localStorage.getItem('name') || '');
+  const get_data = async () =>{
+   // const response= await fetch('http://127.0.0.1:8000/');
+   const response= await fetch('https://fastapibackend.herokuapp.com/');
+    const data= await response.json();
+    const name= data.name;
+    const text= data.text;
+    setText(text);
+    setName(name);
+
+    if(localStorage.getItem('text')!==text){
+      localStorage.setItem('text','')
+    }
+    if(localStorage.getItem('name')!==name){
+      localStorage.setItem('name','')
+    }
+
+  }
+  useEffect(()=>{
+    get_data();
+
+    localStorage.setItem('name',name)
+    localStorage.setItem('text',text)
+
+    }
+              ,[text,name]
+    );
 
   return (
     <div className='hero-container'>
-     /* <video src='/videos/video-1.mp4' autoPlay loop muted />*/
-      <h1>Andrija Jovanovic</h1>
-      <p>fullstack developer</p>
-      
+      <h1>{name}</h1>
+      <p>{text}</p>
+
     </div>
   );
 }
