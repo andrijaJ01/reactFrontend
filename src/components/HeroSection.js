98d2e538b2dd4a67ac9cf28b65b4ae29cd5311b7
Andrijaj01 <andrijajovanovic001@gmail.com>
2021-03-14 13:30:37 +0100

    Updated website design

---
+++ src/components/HeroSection.js
@@ -0,0 +1,18 @@
+import React ,{useState,useEffect} from 'react';
+import '../App.css';
+import { Button } from './Button';
+import './HeroSection.css';
+
+function HeroSection() {
+
+  return (
+    <div className='hero-container'>
+      <video src='/videos/video-2.mp4' autoPlay loop muted />
+      <h1>Andrija Jovanovic</h1>
+      <p>fullstack developer</p>
+      
+    </div>
+  );
+}
+
+export default HeroSection;
