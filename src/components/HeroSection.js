147b8317579b39db7acb86d4e44c9074bf4bf400
Andrijaj01 <andrijajovanovic001@gmail.com>
2021-03-14 14:06:22 +0100

    netlify.toml added

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,7 +7,7 @@ function HeroSection() {
 
   return (
     <div className='hero-container'>
-      <video src='/videos/video-2.mp4' autoPlay loop muted />
+      <video src='/videos/video-1.mp4' autoPlay loop muted />
       <h1>Andrija Jovanovic</h1>
       <p>fullstack developer</p>
       
