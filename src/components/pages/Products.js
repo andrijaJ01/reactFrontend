0e784bdf2f1fceb63d14bfe29625ba046232e757
Andrijaj01 <andrijajovanovic001@gmail.com>
2021-03-14 22:33:21 +0100

    manifest optimization

--- src/components/pages/Products.js
+++ src/components/pages/Products.js
@@ -6,46 +6,46 @@ import CardItem from '../CardItem';
 
 export default function Products() {
   return(
-  <div className='cards'>
-    <h1>My Projects</h1>
-    <div className='cards__container'>
-      <div className='cards__wrapper'>
-        <ul className='cards__items'>
-          <CardItem
-            src='images/img-9.webp'
-            text='Explore the hidden waterfall deep inside the Amazon Jungle'
-            label='Adventure'
-            path='/services'
-          />
-          <CardItem
-            src='images/img-2.webp'
-            text='Travel through the Islands of Bali in a Private Cruise'
-            label='Luxury'
-            path='/services'
-          />
-        </ul>
-        <ul className='cards__items'>
-          <CardItem
-            src='images/img-3.webp'
-            text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
-            label='Mystery'
-            path='/services'
-          />
-          <CardItem
-            src='images/img-4.webp'
-            text='Experience Football on Top of the Himilayan Mountains'
-            label='Adventure'
-            path='/products'
-          />
-          <CardItem
-            src='images/img-8.webp'
-            text='Ride through the Sahara Desert on a guided camel tour'
-            label='Adrenaline'
-            path='/sign-up'
-          />
-        </ul>
+    <div className='cards'>
+      <h1>My Projects!</h1>
+      <div className='cards__container'>
+        <div className='cards__wrapper'>
+          <ul className='cards__items'>
+            <CardItem
+              src='images/portfolio_1.webp'
+              text='Search engine results scrapper'
+              label='Python'
+              path='/services'
+            />
+            <CardItem
+              src='images/portfolio_2.webp'
+              text='Reddit and Discord bots'
+              label='Automation'
+              path='/services'
+            />
+          </ul>
+          <ul className='cards__items'>
+            <CardItem
+              src='images/portfolio_3.webp'
+              text='First iteration of this website made with fastAPI and bootstrap'
+              label='Web'
+              path='/services'
+            />
+            <CardItem
+              src='images/img-4.webp'
+              text='Lorem ipsum project'
+              label='Adventure'
+              path='/products'
+            />
+            <CardItem
+              src='images/img-8.webp'
+              text='Lorem ipsum project No 2'
+              label='Adrenaline'
+              path='/sign-up'
+            />
+          </ul>
+        </div>
       </div>
     </div>
-  </div>
 );
 }
