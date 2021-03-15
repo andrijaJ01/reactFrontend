cd633b187a129774f4652b4cd91fe1ae37d23ed0
Andrijaj01 <andrijajovanovic001@gmail.com>
2021-03-15 22:55:28 +0100

    about page rework

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -1,29 +1,31 @@
 import React,{useState,useEffect} from 'react';
 import '../../App.css';
-import '../Cards.css';
-import CardItem from '../CardItem';
 import Footer from '../Footer';
+import { Chrono } from "react-chrono";
+
 
 export default function Services() {
+
+	
   const [data,setData]= useState({});
   const [title,setTitle]=useState(localStorage.getItem('title') || '');
   const [desc,setDesc]=useState(localStorage.getItem('description') || '');
   const get_data = async () =>{
-    //const response= await fetch('http://127.0.0.1:8000/about');
-    const response= await fetch('https://fastapibackend.herokuapp.com/about');
-    const data= await response.json();
-    const title= data.title;
-    const desc= data.description;
-    setDesc(desc);
-    setTitle(title);
+  const response= await fetch('http://127.0.0.1:8000/about');
+  //const response= await fetch('https://fastapibackend.herokuapp.com/about');
+  const data= await response.json();
+  const title= data.title;
+  const desc= data.description;
+  setDesc(desc);
+  setTitle(title);
 
-    if(localStorage.getItem('title')!==title){
+
+  if(localStorage.getItem('title')!==title){
       localStorage.setItem('title','')
     }
-    if(localStorage.getItem('description')!==desc){
+  if(localStorage.getItem('description')!==desc){
       localStorage.setItem('description','')
     }
-    
   }
   useEffect(()=>{
     get_data();
@@ -38,8 +40,14 @@ export default function Services() {
 	  <>
 	  <h1 className='services'>{title}</h1>
 	  <div className='container'>
-	  {desc}
-
+<Chrono mode="VERTICAL" hideControls theme={{primary: "#242424", secondary: "#222222" , cardForeColor: "violet" }} allowDynamicUpdate>
+    <div>
+      <p className='tline-txt'>{desc.split('$')[0]}</p>
+    </div>
+    <div>
+      <p className='tline-txt'>{desc.split('$')[1]}</p>
+    </div>
+  </Chrono>
 	  </div>
 	<Footer/>
 	  </>
