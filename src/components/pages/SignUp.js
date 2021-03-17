2f952314426436a2c75a456d8ef0a05f8bc61aa6
Andrijaj01 <andrijajovanovic001@gmail.com>
2021-03-17 22:05:45 +0100

    about page rework

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import '../../App.css';
-
+import './grid.css'
+import Footer from '../Footer'
+import emailjs from 'emailjs-com' 
 export default function SignUp() {
-	//window.alert('Work in progress')
-  return (
-	  <>
-	<h1 className='sign-up'>CONTACT</h1>
-	  </>
-	  )
-}
+	  function sendEmail(e) {
+    e.preventDefault();
 
+    emailjs.sendForm('service_2sw5pcy', 'template_sftej7r', e.target,'user_j7DBhn4Od86y8f00Vk84p')
+      .then((result) => {
+          console.log(result.text);
+	  alert('email has been sent')
+      }, (error) => {
+          console.log(error.text);
+	  alert('whoops! there was an error')
+      });
+  }
+
+  return (
+<div>
+    <form className="contact-form" onSubmit={sendEmail}>
+      <input type="hidden" name="contact_number" />
+      <label>Name</label>
+      <input type="text" name="user_name" />
+	  <br/>
+      <label>Email</label>
+      <input type="email" name="user_email" />
+	  <br/>
+      <label>Message</label>
+      <textarea name="message" />
+	  <br/>
+      <input type="submit" value="Send" />
+    </form></div>
+  );
+}
